f6eb89774d63b45836167f2e5d617a9b469cae2c
jeeheezy <jeeho032399@gmail.com>
2024-03-20 16:52:53 -0500

    Created shared partials, removed individual alerts from indexes, and changed program partial into card format with Boostrap

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,4 @@
 class ApplicationController < ActionController::Base
   skip_forgery_protection
+  before_action :authenticate_user!
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,12 @@
 Rails.application.routes.draw do
+  root "programs#index"
   resources :rankings
   resources :pairings
   resources :participations
   resources :programs
   devise_for :users
+
+  get ":username" => "users#show", as: :user
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
