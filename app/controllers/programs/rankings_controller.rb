665c7749da98692472a5cc3f68b011af34ee9728
jeeheezy <jeeho032399@gmail.com>
2024-04-01 12:11:19 -0500

    Adjusted routes and working on params logic for rankings

--- app/controllers/programs/rankings_controller.rb
+++ app/controllers/programs/rankings_controller.rb
@@ -1,13 +1,18 @@
 class Programs::RankingsController < ApplicationController
-  before_action :set_program, only: %i[update destroy]
+  before_action :set_program, only: %i[create update destroy]
 	before_action :set_participation
 	
 	def create
+    rankings_params
+    debugger
 		@rankings = 5.times.map { @participation.rankings.build }
-		@mentor_options = mentor_options(@program)
+    @ranking = Ranking.new()
   end
 
   def update
+    # currently ranks are not explicitly taken from params, but indexed
+    # do I need to permit rank?
+    debugger
     if @participation.rankings.update(rankings_params)
       redirect_to @participation.program, notice: 'Rankings updated successfully.'
     else
@@ -17,6 +22,7 @@ class Programs::RankingsController < ApplicationController
 
 	private
 	def set_program
+    debugger
 		@program = Program.find(params[:program_id])
 	end
 
@@ -25,6 +31,12 @@ class Programs::RankingsController < ApplicationController
 	end
 
   def rankings_params
-    params.require(:rankings).map { |rp| rp.permit(:mentor_id, :rank) }
+    debugger
+    params.require(:ranking).permit(:mentor_id)
+    # params.require(:ranking).map { |rp| rp.permit(:mentor_id, :rank) }
   end
 end
+
+#<ActionController::Parameters {"authenticity_token"=>"1MtXFuwt8ZwKaUWHaBQsPlT1awDD5Qsp_sWu5L6wREekzui5dFfALpH4G11EAxy5z_QzePuFxAXkkonT6_RpbQ", 
+# "ranking"=>{"0"=>{"mentor_id"=>""}, "1"=>{"mentor_id"=>""}, "2"=>{"mentor_id"=>""}, "3"=>{"mentor_id"=>""}, "4"=>{"mentor_id"=>""}}, 
+# "commit"=>"Submit Rankings", "controller"=>"programs/rankings", "action"=>"create", "program_id"=>"1"} permitted: false>
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,12 @@
 Rails.application.routes.draw do
   root "programs#index"
-  resources :rankings
+  # resources :rankings
   resources :pairings
   resources :participations 
   resources :programs do
-    resources :rankings, only: %i[create destroy update]
+    # resources :rankings, only: %i[create destroy update]
+    post "/create" => "programs/rankings#create", as: :create_rankings
+    patch "/update" => "programs/rankings#update", as: :update_rankings
   end
   devise_for :users
 
