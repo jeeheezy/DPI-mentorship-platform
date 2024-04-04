63e1c0c4390337a689b4ac952551776da3e88ab9
jeeheezy <jeeho032399@gmail.com>
2024-04-04 16:25:03 -0500

    Adjusted devise sign up to include custom user attributes

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,11 @@
 class ApplicationController < ActionController::Base
   skip_forgery_protection
   before_action :authenticate_user!
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name, :bio, :preferred_timezone])
+  end
 end
