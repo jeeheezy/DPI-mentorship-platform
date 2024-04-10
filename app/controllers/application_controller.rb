f1d8c2ab4f1790b26fac17d0b6ca3bf9dab7c4ef
jeeheezy <jeeho032399@gmail.com>
2024-04-10 00:59:38 -0500

    Initial pundit inclusion and rescue in ApplicationController

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,10 @@
 class ApplicationController < ActionController::Base
   skip_forgery_protection
+  include Pundit
   before_action :authenticate_user!
   before_action :configure_permitted_parameters, if: :devise_controller?
+  
+  # after_action :verify_authorized, unless: :devise_controller?
 
   protected
 
@@ -9,4 +12,12 @@ class ApplicationController < ActionController::Base
     devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name, :bio, :preferred_timezone, :profile_picture])
     devise_parameter_sanitizer.permit(:account_update, keys: [:first_name, :last_name, :bio, :preferred_timezone, :profile_picture])
   end
+
+  rescue from Pundit::NotAuthorizedError, with: :user_not_authorized
+
+  private
+    def user_not_authorized
+      flash[:alert] = "You are not authorized to perform this action."
+      redirect_back fallback_location: root_url
+    end
 end
