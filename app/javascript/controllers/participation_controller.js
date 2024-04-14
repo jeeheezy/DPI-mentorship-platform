64c73aa91c58e6dfbe4eecec7e018f4ff27ac749
jeeheezy <jeeho032399@gmail.com>
2024-04-14 13:40:21 -0500

    Added button for choosing role and added detail to program show

---
+++ app/javascript/controllers/participation_controller.js
@@ -0,0 +1,24 @@
+import { Controller } from "@hotwired/stimulus"
+
+export default class extends Controller {
+  static targets = [ "role", "questions", "allRoleCards" ]
+  assignRole(event) {
+    const roleSelect = this.roleTarget
+    const chosenRoleDiv = event.currentTarget
+    const chosenRoleDivContent = chosenRoleDiv.querySelector(".role-text").textContent.toLowerCase()
+    const roleCards = this.allRoleCardsTargets
+
+    roleSelect.value = chosenRoleDivContent
+
+    if (chosenRoleDivContent === "mentor") {
+      this.questionsTarget.style.display = "block"
+    } else {
+      this.questionsTarget.style.display = "none"
+    }
+
+    roleCards.forEach(roleCard => {
+      roleCard.classList.remove("chosen-role")
+    })
+    chosenRoleDiv.classList.add("chosen-role")
+  }
+}
